471d4df677473a66959c52c471d67f2911e3cf2c
umashankar <umashankarp33@gmail.com>
2025-07-03 12:47:49 +0530

    add server 2

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 import { createServer } from "http";
 
+//callback-req handler function
+//req,res--objects
 let server=createServer((req,res)=>{
     res.end("Hello world")
 })
 
+//starts a server so requests can be listened on specified port
 server.listen(3000,()=>{
     console.log("Server on Port 3000...");
 })
+
+
+//http--default port -80
+//https--default port -443
