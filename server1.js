cf5d3b17759cf234a8a067a0b45cb4395686bb31
umashankar <umashankarp33@gmail.com>
2025-07-05 12:25:31 +0530

    add third package

--- server1.js
+++ server1.js
@@ -1,5 +1,7 @@
 import { createServer } from "http";
 import fs from "fs";
+import {parse} from 'querystring'
+import { MongoClient } from "mongodb";
 
 let server = createServer((req, res) => {
   if(req.method==="POST"){
@@ -8,30 +10,42 @@ let server = createServer((req, res) => {
         req.on("data",(chunk)=>{
             data+=chunk;
         })
-        req.on("end",()=>{
-            console.log(data);
-            res.end("Thank you for filling the form,We'll get in touch with you soon!!")
+        req.on("end",async ()=>{
+            console.log(parse(data)); 
+
+            let client=new MongoClient("mongodb://127.0.0.1:27017")
+            await client.connect()
+            const db = await client.db("feedbackDB");
+            const collection = await db.collection('feedback');
+            await collection.insertOne(parse(data))
+            
+            res.end("<h1>Thank you for contacting us! We'll get in touch with you..</h1>");
         })
     }else{
         res.end("Invalid data")
     }
+    
   }else{
     if (req.url === "/") {
     res.writeHead(200, "Ok", { "content-type": "text/html" });
     let html = fs.readFileSync("./home.html", "utf-8");
     res.end(html);
+    
   } else if (req.url === "/about") {
     res.writeHead(200, "Ok", { "content-type": "text/html" });
     let html = fs.readFileSync("./about.html", "utf-8");
     res.end(html);
+
   } else if (req.url === "/css") {
     res.writeHead(200, "Ok", { "content-type": "text/css" });
     let css = fs.readFileSync("./style.css", "utf-8");
     res.end(css);
+
   } else if (req.url === "/contact") {
     res.writeHead(200, "Ok", { "content-type": "text/html" });
     let html = fs.readFileSync("./contact.html", "utf-8");
     res.end(html);
+
   } else {
     res.writeHead(404, "not found", { "content-type": "text/plain" });
     res.end("Page not found");
