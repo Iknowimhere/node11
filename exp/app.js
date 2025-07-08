0575bf3c171ee8c93d985d7300656e97c9c745de
umashankar <umashankarp33@gmail.com>
2025-07-08 12:32:37 +0530

    add middlewares

--- exp/app.js
+++ exp/app.js
@@ -2,12 +2,32 @@ import express from 'express'
 //creates an express application, app instance
 let app=express()
 
-app.get("/home",(req,res,next)=>{
+//middlewares
+function parse(req,res,next){
+    console.log("parse function");
+    next()
+}
+function auth(req,res,next){
+    console.log("auth function");
+    next()
+}
+
+
+
+// app.use(parse)
+// app.use(function(req,res,next){
+//     console.log("parse function");
+//     next()
+// })
+// app.use(auth)
+// app.get("/home",parse,auth,routeFunction)
+
+
+let middlewareStack=[parse,auth]
+
+app.get("/home",middlewareStack,function (req,res,next){
     res.status(200).send("this is home page")
 })
 
-app.get("/about",(req,res,next)=>{
-    res.status(200).send("this is about page")
-})
 
 export default app;
